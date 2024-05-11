a044691334a0a1be0968407e4a69989935d8a48a
erikquijano <63459405+erikquijano@users.noreply.github.com>
2024-05-11 14:20:14 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,42 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# Create session state variables for managing login status and user session
+if 'login_status' not in st.session_state:
+    st.session_state['login_status'] = False
 
+def login(username, password):
+    # Simple check for username and password
+    if username == "admin" and password == "testuser20":
+        st.session_state['login_status'] = True
+    else:
+        st.session_state['login_status'] = False
+        st.error("Incorrect Username or Password")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+def logout():
+    st.session_state['login_status'] = False
 
-    st.write("# Welcome to Streamlit! 👋")
+# Layout for login
+def main():
+    st.title("Streamlit Application")
 
-    st.sidebar.success("Select a demo above.")
+    # Check if the user is logged in
+    if st.session_state['login_status']:
+        st.success("Logged in as admin")
+        st.button("Logout", on_click=logout)
+        # Your application code goes here
+        st.write("Welcome to the secure part of the app!")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    else:
+        # Login form
+        with st.form("login_form"):
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+            login_button = st.form_submit_button("Login", on_click=login, args=(username, password))
+            if login_button:
+                if st.session_state['login_status']:
+                    st.success("Login Successful!")
+                else:
+                    st.error("Login Failed. Try Again!")
 
 if __name__ == "__main__":
-    run()
+    main()
